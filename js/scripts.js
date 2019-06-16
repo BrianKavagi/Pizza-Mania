0fb14a5aa8e5354decaee0226fa8da2d2cdee0d2
BrianKavagi <briankavagi06@gmail.com>
2019-06-16 11:47:58 +0300

    Toppings

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,18 @@ function submit(){
 var Pizza size = $("#Pizza size");
 var flavor = $("input:radio[name=flavor]:checked").val();
 var Toppings = $("#Toppings").val();
+if(Pizza size="Large") {
+alert("Total 1000");
+}
+if(Pizza size="Medium") {
+alert("Total 800");
+}
+if(Pizza size="Small") {
+alert("Total 500");
+}
+$(document).ready(function() {
+$("form#submit").submit(function(event) {
+event.preventDefault();
+$("#output").text(result);
+});
+});
